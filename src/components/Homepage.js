d150e8a9e1da7aae781598195f0f677bc718971e
Sam Thompson <electronic62794@yahoo.com>
2019-05-29 21:52:51 -0700

    added content to cards, palyed with colors

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,14 +5,12 @@ import { Container, Row, Col } from "../components/Grid";
 
 const Homepage = () => (
   <div className="home-landing">
-    <h1>Trading Post</h1>
+    <h2>Trading Post</h2>
     <Container>
       <Row>
         <BasicCard>
           <h6 >
-            {" "}
-            An App that is used to help people trade with one another without
-            using money.{" "}
+            Trading post is a bartering platform where people can trade goods without the use of money. 
           </h6>
         </BasicCard>
       </Row>
@@ -22,22 +20,34 @@ const Homepage = () => (
       <Row>
         <Col size="l4 m6 s12">
           <BasicCard>
-          <h2 className="cardTitle">1th Card</h2>
+            <div className="homeCard">
+          <h2 className="cardTitle">Create an account</h2>
+          <p>When you create an account with us, this allows you to post items, and also save them to your wishlist if you don't have anything to trade at the moment</p>
+          </div>
           </BasicCard>
         </Col>
         <Col size="l4 m6 s12">
           <BasicCard>
-          <h2 className="cardTitle">2nd Card</h2>
+          <div className="homeCard">
+          <h2 className="cardTitle">View, save, and create posts</h2>
+          <p>See other people's posts, save the ones you like for later, and create your owns </p>
+          </div>
           </BasicCard>
         </Col>
         <Col size="l4 m6 s12">
           <BasicCard>
-          <h2 className="cardTitle">3nd Card</h2>
+          <div className="homeCard">
+          <h2 className="cardTitle">Contact users, and trade!</h2>
+          <p>If you see something you like, you can message the person who posted it, and if you can work out a deal, meet up and trade!</p>
+          </div>
           </BasicCard>
         </Col>
       </Row>
       <Link to="/signup" className="btn btn-primary">
         Sign up here
+      </Link>
+      <Link to="/trading-post" className="btn btn-primary">
+      Enter Site
       </Link>
     </Container>
   </div>

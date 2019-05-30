f8f61c64e9aee901e271b3b0c2b9409c2c0b4eb4
Sam Thompson <electronic62794@yahoo.com>
2019-05-29 20:47:13 -0700

    added home page cards and titles, figured out basic card reusable component

--- src/components/DevCard/index.js
+++ src/components/DevCard/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import BasicCard from '../BasicCard';
 
 const cardImageStyle = {
     height: "200px",
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,20 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import BasicCard from "./BasicCard";
+import { Container, Row, Col } from "../components/Grid";
 
 const Homepage = () => (
- 
-    
-      <div className="home-landing">
-        <h1>Trading Post</h1>
-        <h4> An App that is used to help people trade with one another without using money </h4>
-        <Link to="/signup" className="btn btn-primary">
-          Sign up here
-        </Link>
-      </div>
-
-  
- 
+  <div className="home-landing">
+    <h1>Trading Post</h1>
+    <Container>
+      <Row>
+        <BasicCard>
+          <h6 >
+            {" "}
+            An App that is used to help people trade with one another without
+            using money.{" "}
+          </h6>
+        </BasicCard>
+      </Row>
+      <Row>
+        <h3>Features</h3>
+      </Row>
+      <Row>
+        <Col size="l4 m6 s12">
+          <BasicCard>
+          <h2 className="cardTitle">1th Card</h2>
+          </BasicCard>
+        </Col>
+        <Col size="l4 m6 s12">
+          <BasicCard>
+          <h2 className="cardTitle">2nd Card</h2>
+          </BasicCard>
+        </Col>
+        <Col size="l4 m6 s12">
+          <BasicCard>
+          <h2 className="cardTitle">3nd Card</h2>
+          </BasicCard>
+        </Col>
+      </Row>
+      <Link to="/signup" className="btn btn-primary">
+        Sign up here
+      </Link>
+    </Container>
+  </div>
 );
 
 export default Homepage;

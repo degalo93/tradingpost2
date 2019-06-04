0160d8896037534c36697cace81bf173c65e88ef
degalo93 <denisgalo10@gmail.com>
2019-06-04 12:46:37 -0700

    Updates

---
+++ precache-manifest.5fc0fabd2e1e28a58c5dd7500a14c4ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "154fe9c78a52eef7747d182dcd25ea70",
+    "url": "/tradingpost2/index.html"
+  },
+  {
+    "revision": "93ecd957e056914b4018",
+    "url": "/tradingpost2/static/css/main.d475c2cd.chunk.css"
+  },
+  {
+    "revision": "bfcb1e0ea4ea9e4b9a63",
+    "url": "/tradingpost2/static/js/2.b76d8686.chunk.js"
+  },
+  {
+    "revision": "93ecd957e056914b4018",
+    "url": "/tradingpost2/static/js/main.fe679404.chunk.js"
+  },
+  {
+    "revision": "1e83c45f372915d74bc2",
+    "url": "/tradingpost2/static/js/runtime~main.fc807996.js"
+  }
+]);
